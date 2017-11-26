d0a37adfe017b2be7cc929edc0b7e6977d5504a3
Dexter Ford <dexter.ford.77@gmail.com>
2017-11-25 18:52:25 -0600

    init

--- js/controllers/stocks_controller.js
+++ js/controllers/stocks_controller.js
@@ -8,24 +8,24 @@ sim.controller("StocksCtrl", ['$scope', 'StockService', 'DateService',
     $scope.getStocks = function() {
       StockService.all().then(function(stocks){
         $scope.stocks = stocks;
+        $scope.thisStock = stocks["2017-10-01"]["price"];
       });
     };
 
     $scope.$watch(function() {return DateService.getCurrentDate()}, function(newValue) {
       $scope.currentDate = newValue;
-      $scope.reformatCurrentDate();
+      reformatCurrentDate();
     });
 
-    $scope.reformatCurrentDate = function() {
+    var reformatCurrentDate = function() {
       var arr = $scope.currentDate.split("/");
       if (arr[1].length === 1) {
         arr[1] = "0" + arr[1];
       }
       $scope.reformattedCurrentDate = arr[2] + "-" + arr[0] + "-" + arr[1];
+      if ($scope.stocks) {
+        $scope.thisStock = $scope.stocks[$scope.reformattedCurrentDate]["price"];
+      }
     };
-
-    $scope.$watch('reformattedCurrentDate', function() {
-      $scope.thisStock = $scope.stocks["Time Series (Daily)"][$scope.reformattedCurrentDate]["4. close"];
-    });
 
 }]);
--- js/services/stock_service.js
+++ js/services/stock_service.js
@@ -5,12 +5,57 @@ sim.service('StockService', ['$http',
 
     this.all = function() {
       if(!_stocks) {
-        var endpoint = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=compact&apikey=4IFXYSZXA7LMMAGK";
+        var endpoint = "data/stock_data.json";
         return $http.get(endpoint).then(function(response){
           _stocks = response.data;
+          _stocks = reformat_stocks(_stocks);
+          _stocks = add_missing_october_dates(_stocks);
           return _stocks
         })
+      } else {
+        return new Promise(function(resolve){ resolve(_stocks) })
       }
     }
-    
+
+    var reformat_stocks = function(stocks) {
+      var reformatted_stocks = {};
+      stocks = stocks["Time Series (Daily)"];
+      for (var key in stocks) {
+        reformatted_stocks[key] = {
+          "price": stocks[key]["4. close"]
+        }
+      }
+      return reformatted_stocks
+    };
+
+    var add_missing_october_dates = function(stocks) {
+      for (var i = 1; i <= 31; i++) {
+        if (i < 10) {
+          var date_to_check = "2017-10-0" + i
+        } else {
+          var date_to_check = "2017-10-" + i
+        }
+        if (!stocks[date_to_check]) {
+          stocks[date_to_check] = {
+            "price": fake_price_data(stocks, date_to_check)
+          }
+        }
+      } 
+      return stocks
+    };
+
+    var fake_price_data = function(stocks, date) {
+      if (date === "2017-10-01") {
+        return stocks["2017-09-29"]["price"]
+      } else {
+        var previous_day = Number(date.split("-")[2]) - 1;
+        if (previous_day < 10) {
+          previous_day = "0" + previous_day.toString();
+        } else {
+          previous_day = previous_day.toString();
+        }
+        return stocks["2017-10-" + previous_day]["price"]
+      }
+    }
+
 }]);
